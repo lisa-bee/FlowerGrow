1072dc5908850078ad89443b86b5338623365d70
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-12-18 20:10:37 +0100

    Interface flowers, update method

--- sketch/flower.ts
+++ sketch/flower.ts
@@ -1,38 +1,57 @@
-let flower75: p5.Image;
-let flowerX = 187.5;
+interface Flowers {
+    bud: p5.Image,
+    flower0: p5.Image,
+    flower25: p5.Image,
+    flower75: p5.Image,
+    flower100: p5.Image,
+}
 
 class Flower {
-    private flower75: p5.Image;
+    private flower: p5.Image;
     private x: number;
     private y: number;
     private width: number;
     private height: number;
+    private time: number;
 
-    public constructor(flower75: p5.Image, x: number, y: number, width: number, height: number) {
-        this.flower75 = flower75;
+    public constructor(x: number, y: number, width: number, height: number) {
+        this.flower = flowers.bud;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.time = 0
     }
 
-    public move() {
+    public update() {
+        this.move()
+        this.grow()
+    }
+
+    private grow() {
+        this.time += deltaTime
+
+        if (this.time > 5000) {
+            this.flower = flowers.flower75
+        }
+    }
+
+    private move() {
         if (keyIsDown(LEFT_ARROW)) {
-            flowerX = flowerX - 6;
+            this.x -= 6;
         }
         else if (keyIsDown(RIGHT_ARROW)) {
-            flowerX = flowerX + 6;
+            this.x += 6;
         }
-        if (flowerX > 375) {
-            flowerX = 375;
+        if (this.x > width - this.width) {
+            this.x = width - this.width;
         }
-        if (flowerX < 0) {
-            flowerX = 0;
+        if (this.x < 0) {
+            this.x = 0;
         }
     }
 
     public draw() {
-        const { flower75, x, y, width, height } = this;
-        image(flower75, x, y, width, height);
+        image(this.flower, this.x, this.y, this.width, this.height);
     }
 }
