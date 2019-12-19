6ff9ddceadd41fe8f1bcb397edba0f7e5fcdc08b
Lisa <lisa.bengtsson@live.com>
2019-12-19 12:17:51 +0100

    cleaned code, removed grassY + potY

--- sketch/ground.ts
+++ sketch/ground.ts
@@ -1,9 +1,6 @@
 let bg: p5.Image;
 let potImg: p5.Image;
 let grassImg: p5.Image;
-let grassY: number = 500;
-let potY: number = 465;
-
 class Grass {
 
     public grassImg: p5.Image;
@@ -23,16 +20,15 @@ class Grass {
     }
 
     public draw() {
-       return image(this.grassImg, this.x, grassY, this.width, this.height);
+       return image(this.grassImg, this.x, this.y, this.width, this.height);
     }
 
     public update() {
         this.move()
     }
     
-
     private move() {
-        grassY = grassY + 1;
+        this.y = this.y + 1;
     }
 
 }
@@ -56,7 +52,7 @@ class Pot {
 
     public draw() {
 
-        return image(this.potImg, this.x, potY, this.width, this.height)
+        return image(this.potImg, this.x, this.y, this.width, this.height)
     }
 
     public update() {
@@ -64,8 +60,7 @@ class Pot {
     }
     
         private move() {
-        potY = potY + 1;
+        this.y = this.y + 1;
     }
     
-
 }
