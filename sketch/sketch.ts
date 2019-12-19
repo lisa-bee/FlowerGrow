b0538686d544168befd7022ca7c25dadf9dff6b9
Lisa <lisa.bengtsson@live.com>
2019-12-19 11:12:40 +0100

    last flower update working fine

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -32,12 +32,7 @@ function setup() {
     createCanvas(400, 600);
     frameRate(60);
     noCursor();
-<<<<<<< HEAD
-    /* fullscreen(); */
-    game = new gameArea()
-=======
     game = new GameArea()
->>>>>>> 8b86ad26b77ec843a211a2d469ab9fe8a5ca31ff
 
 }
 
