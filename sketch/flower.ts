8fc0ab63a46186dd53a0ae78fd6caef89b20a052
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-12-27 12:27:52 +0100

    Work in progress

--- sketch/flower.ts
+++ sketch/flower.ts
@@ -9,6 +9,8 @@ interface Flowers {
 class Flower {
     public currentFlower: p5.Image;
     private time: number;
+    private x: number;
+    private y: number;
     public readonly r: number;
     private history: p5.Vector[];
     private onlyRenderEachXPoint: number;
@@ -18,9 +20,9 @@ class Flower {
         this.currentFlower = listOfFlowers.bud;
         this.time = 0;
         this.r = 36;
-        this.onlyRenderEachXPoint = 50;
+        this.onlyRenderEachXPoint = 35;
         this.keepSamePointsForDifferentDrawsAdjuster = this.onlyRenderEachXPoint;
-        this.history = [createVector(x, y)];
+        this.history = [];
     }
 
     public get beginningOfStem() {
@@ -34,7 +36,7 @@ class Flower {
     public update() {
         const newX = this.handlePlayerInput();
         this.grow(newX);
-        this.move();
+        // this.move();
     }
 
     private grow(x: number) {
@@ -54,11 +56,11 @@ class Flower {
         }
     }
 
-    private move() {
-        for (const point of this.history) {
-            point.y += 1.5;
-        }
-    }
+    // private move() {
+    //     for (const point of this.history) {
+    //         point.y += 1.5;
+    //     }
+    // }
 
     private handlePlayerInput(): number {
         let x = this.endOfStem.x
@@ -81,6 +83,7 @@ class Flower {
 
     private resolveHistoryPositionsToDraw(): Array<p5.Vector> {
         const pointsToDraw = [];
+        pointsToDraw.push(createVector(this.beginningOfStem.x, 600));
         pointsToDraw.push(this.beginningOfStem);
         for (let i = this.keepSamePointsForDifferentDrawsAdjuster % this.onlyRenderEachXPoint; i < this.history.length; i += this.onlyRenderEachXPoint) {
             pointsToDraw.push(this.history[i]);
@@ -95,34 +98,31 @@ class Flower {
     }
 
     public draw() {
-        //console.log("-------- draw ------------");
         stroke(100, 215, 46)
         strokeWeight(6);
         noFill();
 
-        const maxNumberOfPointsInCurveVertex = 4;
+        // const maxNumberOfPointsInCurveVertex = 4;
 
         const historyPositionsToDraw = this.resolveHistoryPositionsToDraw();
-        if (historyPositionsToDraw.length >= 2) {
-            for (let i = 0; i < historyPositionsToDraw.length; i++) {
-                beginShape();
-                if (i === 0) {
-                    curveVertex(historyPositionsToDraw[0].x, historyPositionsToDraw[0].y);
-                    curveVertex(historyPositionsToDraw[0].x, historyPositionsToDraw[0].y);
-                    curveVertex(historyPositionsToDraw[1].x, historyPositionsToDraw[1].y);
-                    curveVertex(historyPositionsToDraw[1].x, historyPositionsToDraw[1].y);
-                }
-                else {
-                    for (let j = 0; j < maxNumberOfPointsInCurveVertex; j++) {
-                        if (i + j < historyPositionsToDraw.length) {
-                            const pos = historyPositionsToDraw[i + j];
-                            curveVertex(pos.x, pos.y);
-                        }
-                    }
-                }
-                endShape();
+        beginShape();
+        for (let i = 0; i < historyPositionsToDraw.length; i++) {
+            if (i === 0) {
+                curveVertex(historyPositionsToDraw[0].x, historyPositionsToDraw[0].y);
+                curveVertex(historyPositionsToDraw[0].x, historyPositionsToDraw[0].y);
+                curveVertex(historyPositionsToDraw[1].x, historyPositionsToDraw[1].y);
+                curveVertex(historyPositionsToDraw[1].x, historyPositionsToDraw[1].y);
+            }
+            else {
+                // for (let j = 0; j < maxNumberOfPointsInCurveVertex; j++) {
+                //     if (i + j < historyPositionsToDraw.length) {
+                //         const pos = historyPositionsToDraw[i + j];
+                curveVertex(historyPositionsToDraw[i].x, historyPositionsToDraw[i].y);
+                // }
+                // }
             }
         }
+        endShape();
 
         push();
         imageMode(CENTER);
@@ -136,3 +136,13 @@ class Flower {
         pop();
     }
 }
+
+// class ArrayUtils {
+//     public static range(start: number, end: number) {
+//         const ans = [];
+//         for (let i = start; i <= end; i++) {
+//             ans.push(i);
+//         }
+//         return ans;
+//     }
+// }
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -50,5 +50,4 @@ function draw() {
     background(bg);
     game.update();
     game.draw();
-    //game.update();
 }
