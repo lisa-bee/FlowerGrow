fa13cf1dd58df6a7b157a940056977836c40f379
Lisa <lisa.bengtsson@live.com>
2020-01-07 13:12:22 +0100

    cleaned code

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -2,15 +2,8 @@ let bg: p5.Image;
 let game: GameArea;
 let listOfFlowers: Flowers;
 let backgroundMusic: p5.SoundFile;
-/**
- * Built in preload function in P5
- * This is a good place to load assets such as
- * sound files, images etc...
- */
+
 function preload() {
-    // Tyvärr har jag inte fått till den globala typningen för
-    // inladdningen av ljud men fungerar bra enligt nedan..
-    // sound = (window as any).loadSound('../assets/mySound.wav');
     bg = loadImage('./assets/images/background400x600.png');
     potImg = loadImage('./assets/images/pot.png');
     grassImg = loadImage('./assets/images/grass.png');
@@ -45,28 +38,14 @@ function preload() {
     }
 }
 
-
-/**
- * Built in setup function in P5
- * This is a good place to create your first class object
- * and save it as a global variable so it can be used
- * in the draw function below
- */
 function setup() {
     createCanvas(400, 600);
     frameRate(60);
-    //GameArea.spawnBee();
-    //noCursor();
     game = new GameArea();
     backgroundMusic.setVolume(0.2);
     backgroundMusic.loop();
 }
 
-/**
- * Built in draw function in P5
- * This is a good place to call public funcions of the object
- * you created in the setup function above
- */
 function draw() {
     background(bg);
     game.update();
